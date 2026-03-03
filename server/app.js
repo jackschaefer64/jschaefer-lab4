76fbf0df95cc84b66fff52b743689e7d5b12d7bb
jackschaefer64 <jackschaefer64@gmail.com>
2026-03-03 12:43:37 -0500

    adding html file and app.js

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.get('/', function (req, res) {
+res.sendFile(path.join(__dirname,'..', 'client', 'views', 'index.html'));
+});
+app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
