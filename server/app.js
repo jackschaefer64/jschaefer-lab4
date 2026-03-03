09d34007d970e0f73a3b1fd7ba82a5754f712e16
jackschaefer64 <jackschaefer64@gmail.com>
2026-03-03 13:58:09 -0500

    started adding images to the html file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.static('client/public'));
+
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname,'..', 'client', 'views', 'index.html'));
-});
+})
 app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
