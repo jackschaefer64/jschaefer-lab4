57f0877939f5baefe78692a49efef0ac2e312ad6
jackschaefer64 <jackschaefer64@gmail.com>
2026-03-10 13:43:38 -0400

    adding cs file, image file, and changed existing files

--- client/public/js/testing.js
+++ client/public/js/testing.js
@@ -1,1 +1,15 @@
-Console.log("Hello World");
+posts = [{title: 'Post1', body: 'Hello World', user: 'Wizard1'}, {title: 'Post2', body: 'Hello World', user: 'Wizard2'}];
+imageList = ["/img/wizard.png", "/img/wizard2.png"];
+const postListContainer = document.getElementById("post-list");
+function loadPosts()
+{
+    for(i = 0; i < posts.length; i++)
+    {
+        console.log(posts[i].user);
+        console.log(imageList[i]);
+    }
+}
+window.onload = function()
+{
+    document.getElementById("myButton").addEventListener("click", loadPosts);
+}
