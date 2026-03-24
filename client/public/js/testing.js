bccaa0e9b19f26bb10ffe48dd252078b40350990
jackschaefer64 <jackschaefer64@gmail.com>
2026-03-24 13:47:46 -0400

    Added proper innerhtml code and started displaying posts

--- client/public/js/testing.js
+++ client/public/js/testing.js
@@ -1,18 +1,29 @@
-posts = [{title: 'Post1', body: 'Hello World', user: 'Wizard1'}, {title: 'Post2', body: 'Hello World', user: 'Wizard2'}];
-imageList = ["/img/wizard.png", "/img/wizard2.png"];
-const postListContainer = document.getElementById("post-list");
+posts = [{title: 'Post1', body: 'Hello World1', user: 'Wizard1', img: "/img/wizard.png"}, 
+        {title: 'Post2', body: 'Hello World2', user: 'Wizard2', img: "/img/wizard2.png"}, 
+        {title: 'Post3', body: 'Hello World3', user: "Wizard3", img: "/img/wizard3.png"}];
+
 function loadPosts()
 {
     for(i = 0; i < posts.length; i++)
     {
         let postText = document.getElementById("post1");
-        postText.innerHTML = posts[i].user;
-        console.log(posts[i].user);
-        console.log(imageList[i]);
+        const postsHtml = posts.map(post => `
+            <div class="post">
+                <h2 class = "title">${post.title}</h2>
+                <p>${post.body}</p>
+                <img src = ${post.img} class = "container"></img>
+            </div>
+        `).join(''); 
+        postText.innerHTML = postsHtml;
     }
+       
 }
-window.onload = function()
+window.addEventListener("load", () =>
 {
     document.getElementById("myButton").addEventListener("click", loadPosts);
 
-}
+});
+/*
+let feedElement = document.getElementById('demo--feed');
+feedElement.innerHtml = "Hello World!";
+*/
