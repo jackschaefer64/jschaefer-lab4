27bd7bca81504bf9ead1f4b81b8bcb80cc7e8ea5
jackschaefer64 <jackschaefer64@gmail.com>
2026-03-27 15:16:04 -0400

    Added more styling to the posts and the button

--- client/public/js/testing.js
+++ client/public/js/testing.js
@@ -1,6 +1,6 @@
-posts = [{title: 'Post1', body: 'Hello World1', user: 'Wizard1', img: "/img/wizard.png"}, 
-        {title: 'Post2', body: 'Hello World2', user: 'Wizard2', img: "/img/wizard2.png"}, 
-        {title: 'Post3', body: 'Hello World3', user: "Wizard3", img: "/img/wizard3.png"}];
+posts = [{title: 'Help with brewing potions?', body: 'Looking for some assistance with brewing up some new potions', user: 'Wizard1', img: "/img/wizard.png", user: "@inmytower"}, 
+        {title: 'Nearby battles?', body: 'Does anyone know of some battles or crusades going on? Trying to practice my spells', user: 'Wizard2', img: "/img/wizard2.png", user: "@spellslinger"}, 
+        {title: 'FREE STAFF', body: 'Found and old staff while cleaning. Up for grabs (not cursed)', user: "Wizard3", img: "/img/wizard3.png", user: "@wiseandwizened"}];
 
 function loadPosts()
 {
@@ -8,10 +8,12 @@ function loadPosts()
     {
         let postText = document.getElementById("post1");
         const postsHtml = posts.map(post => `
-            <div class="post">
-                <h2 class = "title">${post.title}</h2>
-                <p>${post.body}</p>
-                <img src = ${post.img} class = "container"></img>
+            <div>
+                <h2 class = "title">${post.title}<span class = "username">    ${post.user}</span></h2>
+                <div class = "post-container">
+                    <p class = "post-body">${post.body}</p>
+                    <img src = ${post.img} class = "container-image"></img>
+                </div>
             </div>
         `).join(''); 
         postText.innerHTML = postsHtml;
